a1d0b8b3b2d7e9de0460763766daa173696f36d7
Arnob Bokshi <153275698+arnob1001@users.noreply.github.com>
2025-03-24 07:34:42 +0000

    next.config.mjs updated

--- financeflow/next.config.mjs
+++ financeflow/next.config.mjs
@@ -1,23 +1,21 @@
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "randomuser.me" },
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+      },
       { protocol: "https", hostname: "images.pexels.com" },
     ],
   },
 
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "X-Requested-With, Content-Type" },
-        ],
-      },
-    ];
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb",
+    },
   },
 };
 
